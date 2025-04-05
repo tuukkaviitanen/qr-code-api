8d4c9eaffcfbd78e6b5123443455d9bc36eaa152
tuukkaviitanen <tuukka.viitanen@gmail.com>
2025-04-05 21:22:34 +0300

    Added PORT env value

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 use axum::{
     Router,
     extract::Path,
@@ -12,16 +14,20 @@ mod qr_code_generation;
 
 #[tokio::main]
 async fn main() {
+    let port = env::var("PORT").unwrap_or_else(|_| "3000".to_string());
+
     // build our application with a single route
     let app = Router::new().route("/{*content}", get(get_qr_code));
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{port}"))
+        .await
+        .expect(&format!("Failed to bind to port \"{port}\""));
     axum::serve(listener, app).await.unwrap();
 }
 
 async fn get_qr_code(Path(content): Path<String>) -> Result<Response, Error> {
-    let qr_code = qr_code_generation::generate_qr_code(content.as_str()).await?;
+    let qr_code = qr_code_generation::generate_qr_code(&content).await?;
     Ok((
         axum::http::StatusCode::OK,
         [("content-type", "image/svg+xml")],
