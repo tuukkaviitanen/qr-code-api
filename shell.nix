cb3e88d55bca59dd6f9e8cefd0e6499d9eed4309
Tuukka Viitanen <97726090+tuukkaviitanen@users.noreply.github.com>
2025-05-14 22:31:24 +0300

    Added nix-shell support for local development (#10)
    * Added nix-shell support for local development
    
    * Added rustc to nix-shell environment

---
+++ shell.nix
@@ -0,0 +1,13 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let 
+  rust-pkgs = (import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/1cb1c02a6b1b7cf67e3d7731cbbf327a53da9679.tar.gz") {});#1.86.0
+in
+  pkgs.mkShell {
+    packages = with rust-pkgs; [ 
+      rustc
+      cargo
+      rustfmt
+      clippy
+    ];
+  }
